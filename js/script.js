305314254cb15e8e851b5073d18851732ee5664e
Taylor Crane <taylorkcrane@gmail.com>
2015-03-09 22:47:05 -0400

    stuck on the sliding transition

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-	
+	$("#im-ready").on("click", function() {
+		$(".play-game").animate({
+			top: "-40%"
+		}, 500)
+		$(".game-screen").toggle();
+		$(".game-screen").animate({
+			height: "80vh"
+		}, 500, "linear")
+	})
 });
