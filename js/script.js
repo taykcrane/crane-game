8daa38a2e5769a40531a366259363c9e3b4d63c4
Taylor Crane <taylorkcrane@gmail.com>
2015-03-11 22:43:48 -0400

    began building the game logic

--- js/script.js
+++ js/script.js
@@ -1,11 +1,36 @@
+// When the "I'm Ready" button is clicked, the game screen moves into view
+
 $(document).ready(function() {
 	$("#im-ready").on("click", function() {
 		$(".play-game").animate({
 			top: "-40%"
-		}, 500)
-		$(".game-screen").toggle();
+		}, 500, "easeInOutCubic")
+		$(".game-screen").show();
 		$(".game-screen").animate({
-			height: "80vh"
-		}, 500, "linear")
-	})
+			height: "100%"
+		}, 500, "linear", function() {
+			$(".footer").show();
+			$(".footer").animate({
+				bottom: "0"
+			}, 1000, "easeOutElastic")
+		})
+	});	
 });
+
+//My question objects
+var question1 = {
+	qNum: 1,
+	questionText: "Taylor's middle name is:",
+	answers: ["K", "Kaleidescope", "Khaleesi", "He doesn't have one"],
+	correctAnswer: 3
+}
+
+var question2 = {
+	qNum: 2,
+	questionText: "Taylor's favorite burger from Bareburger is:",
+	answers: ["Maui Waui", "Roadhouse", "Western", "Supreme", "Taylor's actually a vegan"],
+	correctAnswer: 0
+}
+
+console.log(question1.qNum);
+console.log(question2.answers);
